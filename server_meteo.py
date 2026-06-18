8048ccf973ac300d54eb2960845117e788da0914
Mihaih29 <161763386+Mihaih29@users.noreply.github.com>
2026-06-18 21:15:05 +0300

    Fix formatting and cleanup comments in server_meteo.py

--- server_meteo.py
+++ server_meteo.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) # Permite site-ului sa acceseze serverul
+CORS(app) 
 
-# Variabila globala unde tinem ultimele date primite
 date_senzori = {
     "temp": 0,
     "hum": 0,
@@ -23,5 +22,5 @@ def get_data():
     return jsonify(date_senzori)
 
 if __name__ == '__main__':
-    # Ruleaza pe IP-ul local al Raspberry Pi pe portul 5000
+
     app.run(host='0.0.0.0', port=5000)
